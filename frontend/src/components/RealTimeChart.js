93cf37ad7252b6b9fabb2eb015803a493b7cd58b
riccardo <scaringi_riccardo@libero.it>
2025-01-28 22:29:50 +0100

    Update RealTimeChart.js

--- frontend/src/components/RealTimeChart.js
+++ frontend/src/components/RealTimeChart.js
@@ -1,1 +1,22 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+const RealTimeChart = ({ data }) => {
+  const chartData = {
+    labels: data.map((d) => d.timestamp),
+    datasets: [
+      {
+        label: "Valore Sensori",
+        data: data.map((d) => d.value),
+        borderColor: "rgba(75,192,192,1)",
+        borderWidth: 2,
+        fill: false,
+      },
+    ],
+  };
+
+  return <Line data={chartData} />;
+};
+
+export default RealTimeChart;
 
