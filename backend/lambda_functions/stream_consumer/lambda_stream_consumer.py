93c0c26f95722c8792e5f099eba25e440de7fbf7
riccardo <scaringi_riccardo@libero.it>
2025-01-28 22:33:25 +0100

    Update lambda_stream_consumer.py

--- backend/lambda_functions/stream_consumer/lambda_stream_consumer.py
+++ backend/lambda_functions/stream_consumer/lambda_stream_consumer.py
@@ -1,1 +1,14 @@
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    for record in event["Records"]:
+        payload = json.loads(record["kinesis"]["data"])
+        logger.info(f"Dati ricevuti: {payload}")
+
+        # Logica di elaborazione del record
+        # Qui puoi implementare ulteriori operazioni sul payload
 
