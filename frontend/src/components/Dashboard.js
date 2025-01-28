d9b30d186e8179af2c528672a15032928f50db53
riccardo <scaringi_riccardo@libero.it>
2025-01-28 22:28:09 +0100

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,1 +1,24 @@
+import React, { useState, useEffect } from "react";
+import RealTimeChart from "./RealTimeChart";
+import DataGeneratorForm from "./DataGeneratorForm";
+
+const Dashboard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/data")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <DataGeneratorForm />
+      <RealTimeChart data={data} />
+    </div>
+  );
+};
+
+export default Dashboard;
 
